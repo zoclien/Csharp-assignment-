48acc4423419284f86ea1874d64159cfc752c23e
zoclien <47891977+zoclien@users.noreply.github.com>
2019-02-24 23:05:48 +0800

    第一次作业1.4

---
+++ 1.4/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            s = Console.ReadLine();
+            double n = Double.Parse(s);
+            s = Console.ReadLine();
+            double m = Double.Parse(s);
+            double e = m * n;
+            Console.WriteLine("m*n="+e);
+       
+        }
+    }
+}
