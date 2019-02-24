9d6d97f7b7199ec725e60bc0b6e13be690accb96
zoclien <47891977+zoclien@users.noreply.github.com>
2019-02-24 23:06:47 +0800

    第一次作业1.5

---
+++ 1.5/multi/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multi
+{
+    public partial class Form1 : Form
+    {
+        public double m, n, x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m = Double.Parse(textBox2.Text);
+            n = Double.Parse(textBox1.Text);
+            x = m * n;
+            label1.Text = "乘积：" + x;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
