89f29b3afa036f4e54528bed5fdf5e09cc52f1ca
zoclien <47891977+zoclien@users.noreply.github.com>
2019-03-05 11:51:25 +0800

    2.9
    2.9

---
+++ 2.9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int S = 2, max = 100, muti = 1;
+            bool []flag = new bool[100];
+            for (int i = 0; i < max; i++)
+                flag[i] = true;
+            for(;S<max;S++)
+            {
+                if (!flag[S - 1])
+                    continue;
+                for (muti = 2; S * muti<=max; muti++)
+                    flag[S * muti - 1] = false;
+
+            }
+            for (int i=0;i<max;i++)
+            {
+                if (flag[i])
+                    Console.WriteLine(i+1+",");
+  
+            }
+            string s=Console.ReadLine();
+
+        }
+    }
+}
