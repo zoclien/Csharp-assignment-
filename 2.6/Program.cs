117abe9b8422106c215b010023327e85936a4e61
zoclien <47891977+zoclien@users.noreply.github.com>
2019-03-05 11:49:53 +0800

    2.6
    2.6

---
+++ 2.6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace su
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 2;
+            double  num;
+           
+            int increase=1;
+            string s;
+            s = Console.ReadLine();
+            num = double.Parse(s);
+            int N = (int)Math.Sqrt(num);
+           while(f<=N)
+            {
+                while (num%f==0)
+                {
+                    Console.WriteLine(f);
+                    num /= f;
+                }
+                if (f == 3)
+                    increase = 2;
+                f += increase;
+
+            }
+            if (num >= f)
+                Console.WriteLine(num);
+           
+        }
+    }
+}
