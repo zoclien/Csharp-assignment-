1060ce967e11e58f67edf1ce7168d1b8a5fe73a7
zoclien <47891977+zoclien@users.noreply.github.com>
2019-03-12 19:40:48 +0800

    prototype

---
+++ prototype/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            massage first = new massage();
+            first.setsender("mrA");
+            first.setreader("mrB");
+            first.setime(180310);
+            first.setdata("how are you");
+            massage firstc = first.clonemassage();
+            firstc.setreader("mrC");
+            Console.WriteLine("first=firstc?");
+            Console.WriteLine(first == firstc);
+            first.read();
+            firstc.read();
+
+        }
+    }
+}
---
+++ prototype/massage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class massage
+{
+    public string sender;
+    public string reader;
+    public int time;
+    public string data;
+    public void setsender(string s)
+    {
+        this.sender = s;
+    }
+    public void setreader(string r)
+    {
+        this.reader = r;
+    }
+    public void setime(int t)
+    {
+        this.time = t;
+    }
+    public void setdata(string d)
+    {
+        this.data = d;
+    }
+    public void read()
+    {
+        Console.WriteLine("发送者：" + this.sender + "接收者：" + this.reader + "日期：" + this.time + "内容：" + this.data);
+    }
+	public massage clonemassage()
+	{
+        massage m = new massage();
+        m.time = this.time;
+        m.reader = this.reader;
+        m.sender = this.sender;
+        m.data = this.data;
+        return m;
+	}
+}
