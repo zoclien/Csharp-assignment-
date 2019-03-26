41f2f292efa9483225f74858a36ae36a921b37bc
zoclien <47891977+zoclien@users.noreply.github.com>
2019-03-26 17:10:30 +0800

    第五次作业

---
+++ ord5/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace ord5
+{
+    class Program
+    {
+        private static orders[] list = new orders[10];
+        private static bool[] listflag = new bool[10];
+        public void init()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                listflag[i] = true;
+
+            }
+        }
+        static void creatorder()
+        {
+            try
+            {
+               
+                for (int i = 0; i < 10; i++)
+                {
+                    if (listflag[i])
+                    {
+                        
+                        orders addo = new orders();
+                        while (true)
+                        {if (addo.CompareTo(list[i]) >= 0)
+                            { list[i] = addo;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            catch (System.OutOfMemoryException)
+            {
+                Console.WriteLine("fail :out of memory");
+            }
+            finally { Console.WriteLine("end"); };
+        }
+        static void delete(int luckguy)
+        {
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (listflag[i] && list[i].number == luckguy)
+                    {
+                        listflag[i] = !listflag[i];
+                        list[i] = null;
+                    }
+                }
+            }
+            catch (System.NullReferenceException);
+            {
+                Console.WriteLine("fail:null reference fail");
+            }
+            }
+            finally { Console.WriteLine("end"); };
+        }
+        static void checkord(int luckguy)
+        {
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    if (listflag[i] && list[i].number == luckguy)
+                        list[i].display();
+            }
+            catch (System.StackOverflowException)
+            {
+                Console.WriteLine("fail");
+            }
+            finally { Console.WriteLine("end"); };
+        }
+        public static void Main(string[] args)
+        {
+            int key;
+            Console.WriteLine("delete:1/check:2/creat:3/");
+            string g = Console.ReadLine();
+            key = int.Parse(g);
+
+
+            if (key == 1)
+            {
+                Console.WriteLine("delete");
+                g = Console.ReadLine();
+                int t = int.Parse(g);
+                delete(t);
+            }
+
+            if (key == 2)
+            {
+                Console.WriteLine("check");
+                g = Console.ReadLine();
+                int c = int.Parse(g);
+                checkord(c);
+            }
+
+            if (key == 3)
+            {
+                Console.WriteLine("creat");
+                creatorder();
+
+            }
+        }
+
+
+
+    }
+}
---
+++ ord5/orders.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class orders:IComparable
+{
+    public int   number;
+    string client;
+    orderdeteils det = new orderdeteils();
+    public int CompareTo(object s)
+    {
+        return number - (s as orders).number;
+    }
+    public override bool Equals(object obj)
+    {
+        var d = obj as orders;
+        return this.det.Equals(d.det) && number == d.number && client == d.client;
+      // return base.Equals(obj);
+    }
+    public override int GetHashCode()
+    {
+        int hashcode = 1522631281;
+        hashcode = hashcode * -1521134295 + number.GetHashCode();
+        hashcode = hashcode * -1521134295 + client.GetHashCode();
+        hashcode = hashcode * -1521134295 + det.GetHashCode();
+        return hashcode;
+        // return base.GetHashCode();
+    }
+    public override string ToString()
+    {
+        return "name:" + client + "oederid:" + number + det.ToString();
+    }
+    public void display()
+    {
+        Console.WriteLine("order number" + this.number + "client name" + this.client);
+    }
+    public orders()
+    {
+        Console.WriteLine("client");
+        this.client = Console.ReadLine();
+        Console.WriteLine("number");
+        string k = Console.ReadLine();
+        this.number = int.Parse(k);
+        this.det = new orderdeteils();
+    }
+}
